c19849af21e6a103494c86c8f0926750fb2e2da2
adkood <ashutoshnegi195@gmail.com>
2024-01-13 03:16:51 +0530

    navbar changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,7 +3,20 @@ import style from '../Navbar/Navbar.module.css';
 const Navbar = () => {
     return (
         <div className={style.nav}>
-            
+            <section className={style.nav__left}>
+                icon
+             </section>
+            <section className={style.nav__right}>
+                <section className={style.nav__option}>
+                    HOME
+                </section>
+                <section className={style.nav__option}>
+                    PRODUCTS                    
+                </section>
+                <section className={style.nav__option}>
+                    CONTACT-US
+                </section>
+            </section>
         </div>
     );
 }
