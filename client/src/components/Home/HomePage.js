dd75fad9f7050e9cec3d5796800007c6dabf4142
adkood <ashutoshnegi195@gmail.com>
2024-01-19 18:20:23 +0530

    home changes 2

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
             <IoMdStar className={style.star} size="2rem" color="white" />
             <div className={style.cont}>
                 <span className={style.content}>
-                    JD Jain and Company proudly presents its manufacturing unit JDJ Alloys as it embarks on a groundbreaking venture
+                    JD Jain and Company proudly presents its manufacturing unit JDJ Alloys as it embarks on a groundbreaking venture.
                 </span>
             </div>
         </div>
