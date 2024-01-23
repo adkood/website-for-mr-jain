82ac7634bbc60d2de986ad6838d6d8736f7f8156
adkood <ashutoshnegi195@gmail.com>
2024-01-23 18:46:52 +0530

    technology structure changed

--- client/src/components/Technology/Technology.js
+++ client/src/components/Technology/Technology.js
@@ -22,13 +22,18 @@ const Technology = () => {
                 <span>COPPER PRODUCTION Technology</span>
             </div>
             <div id="slider-cont" className={styles.sliderCont}>
-                <Frame width="90%" height="75%" image={tech_list[currIdx].image} name={tech_list[currIdx].name} />
-                <section className={styles.dotBox}>
-                    <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '1' ? "green" : "white"}` }}></div>
-                    <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '2' ? "green" : "white"}` }}></div>
-                    <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '3' ? "green" : "white"}` }}></div>
-                    <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '4' ? "green" : "white"}` }}></div>
-                    <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '5' ? "green" : "white"}` }}></div>
+                <section className={styles.sliderCont__left}>
+                    <Frame width="90%" height="75%" image={tech_list[currIdx].image} name={tech_list[currIdx].name} />
+                    <section className={styles.dotBox}>
+                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '1' ? "green" : "white"}` }}></div>
+                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '2' ? "green" : "white"}` }}></div>
+                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '3' ? "green" : "white"}` }}></div>
+                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '4' ? "green" : "white"}` }}></div>
+                        <div style={{ width: "15px", height: "15px", borderRadius: "50%", backgroundColor: `${tech_list[currIdx].id === '5' ? "green" : "white"}` }}></div>
+                    </section>
+                </section>
+                <section className={styles.sliderCont__right}>
+                    <span>{tech_list[currIdx].short}</span>
                 </section>
             </div>
         </div>
--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,1 +1,1 @@
-export const tech_list = [{id: '1', image: 'casting.jpg', name: "Casting"}, {id: '2' , image: 'copperWire.png', name: "wire"}];
+export const tech_list = [{id: '1', image: 'casting.jpg', name: "Casting", short: 'Casting is a manufacturing process in which molten material, like metal, is poured into a mold and allowed to harden.'}, {id: '2' , image: 'copperWire.png', name: "wire", short: 'some wiring thing ssas ibwdsai dwbd.'}];
