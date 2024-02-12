caa7ae8a9f4a5dfb6af285c781ad97383e47ee6f
adkood <ashutoshnegi195@gmail.com>
2024-02-12 23:30:34 +0530

    created about us

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
 import Technology from './components/Technology/Technology';
 import Application from './components/Application/Application';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Products />
       <Technology />
       <Application />
+      <AboutUs />
     </>
   );
 }
---
+++ client/src/components/AboutUs/AboutUs.js
@@ -0,0 +1,21 @@
+import styles from '../AboutUs/AboutUs.module.css';
+import { GiMoebiusStar } from "react-icons/gi";
+
+const AboutUs = () => {
+    return (
+        <div className={styles.aboutUs}>
+            <section className={styles.top}>
+                <GiMoebiusStar size={"1rem"} />
+                <span>About us</span>
+                <GiMoebiusStar size={"1rem"} />
+
+            </section>
+            <section className={styles.bottom}>
+                <span>Established in 1949, JD Jain and Company stands as a stalwart in the copper trading industry. With a rich legacy of over seven d­ecades, we have been the epitome of reliability and excellence in wholesale copper trade. Our commitment to quality and customer satisfaction has propelled us to the forefront of the market, making JDJ & Co. a trusted name in the industry since its inception.
+                    JD Jain and Company proudly presents its manufacturing unit, JDJ Alloys, as it embarks on a groundbreaking venture.</span>
+            </section>
+        </div>
+    );
+}
+
+export default AboutUs; 
