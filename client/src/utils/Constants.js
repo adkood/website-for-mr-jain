82a56c5a6cafb603d319f4348078321e89660e52
adkood <ashutoshnegi195@gmail.com>
2024-02-15 19:38:41 +0530

    content length reduced

--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,45 +1,38 @@
 export const prod_list = [{ id: '1', image: 'https://i.ibb.co/brsSPgv/copper-Wire.png', name: "Copper Wire", short: 'Casting is a manufacturing process in which molten material, like metal, is poured into a mold and allowed to harden.' }, { id: '2', image: 'https://i.ibb.co/tYmD8LG/copper-Rod.png', name: "Copper Rod", short: 'Constructed of copper that could also insulate or shield electric current.' }, { id: '3', image: 'https://i.ibb.co/FsJ8MKS/copper-Strip.png', name: "Copper Strip", short: 'Extrusion is a compressive forming manufacturing process that creates lengths of shaped cross-sections from metal billets.' }];
 export const tech_list = [{
-    id: '1', image: 'https://i.ibb.co/89ym8sG/casting.jpg', name: "Casting", short: 'Casting is a manufacturing process in which molten material, like metal, is poured into a mold and allowed to harden.', advantage: ["Continuous production of soft annealed copper in different shapes",
-        "High dimensional accuracy",
+    id: '1', image: 'https://i.ibb.co/89ym8sG/casting.jpg', name: "Casting", short: 'Casting is a manufacturing process in which molten material, like metal, is poured into a mold and allowed to harden.', advantage: ["Continuous production of soft annealed copper.",
         "Defect free production in continuous length",
         "High electrical conductivity",
         "Production of complex cross sectional geometry"], prod_avail: ["Bare copper strip", "Rectangular wire", "Copper profile"], application: ["Earthing Conductor",
             "Lightening protection",
             "Transformer winding wire",
-            "Profiles for connectors",
-            "Busbars"]
+            "Profiles for connectors"]
 },
 {
     id: '2', image: 'https://i.ibb.co/fxFZd8t/insulation.png', name: "copper insulation", short: 'Constructed of copper that could also insulate or shield electric current.', advantage: ["Advanced technology and state of the art machines",
         "Excellent corner radius and accurate sizes",
         , "Air condition plant to control and avoid dust contamination",
         , "Online quality control for better reliability"
-    ], prod_avail: [`Single, double and triple individual and overall insulated rectangular wire with Kraft paper, Thermally stabilized paper, Mica tape,
-Nomex high temperature polyester etc`,
+    ], prod_avail: [`Single, double and triple individual and overall insulated rectangular wire with Kraft paper, Thermally stabilized paper, Mica tape etc`,
         "Sizes upto 400 S.mm max"], application: ["Oil filled transformer winding",
             "Dry type transformer winding",
-            "Heavy duty motor / Generator",
-            "Traction motors & other equipment"]
+            "Heavy duty motor / Generator"]
 }, {
-    id: '3', image: 'https://i.ibb.co/fDGtqnb/extrusion.png', name: "copper extrusion", short: 'Extrusion is a compressive forming manufacturing process that creates lengths of shaped cross-sections from metal billets.', advantage: ["Continuous production of soft annealed copper in different shapes",
-        "High dimensional accuracy",
-        "Defect free production in continuous length",
-        "High electrical conductivity",
-        "Production of complex cross sectional geometry"], prod_avail: ["Bare copper strip", "Rectangular wire", "Copper profile"], application: ["Earthing Conductor",
-            "Lightening protection",
-            "Transformer winding wire",
-            "Profiles for connectors",
-            "Busbars"]
+    id: '3', image: 'https://i.ibb.co/fDGtqnb/extrusion.png', name: "copper extrusion", short: 'Extrusion is a compressive forming manufacturing process that creates lengths of shaped cross-sections from metal billets.', advantage: ["Continuous production of soft annealed copper.",
+    "Defect free production in continuous length",
+    "High electrical conductivity",
+    "Production of complex cross sectional geometry"], prod_avail: ["Bare copper strip", "Rectangular wire", "Copper profile"], application: ["Earthing Conductor",
+        "Lightening protection",
+        "Transformer winding wire",
+        "Profiles for connectors"]
 }, {
     id: '4', image: 'https://i.ibb.co/8xqq4Ld/drawing2.png', name: "copper drawing", short: 'This process allows artists to create intricate and detailed prints with fine lines and textures.', advantage: ["Suitable for fine wire drawing",
         "High conductivity helps less metal usage",
-        "Longer packing length suitable for welding to make a longer length of finished product",
+        "Longer packing length suitable for welding",
         "Easy to stringing up in wire drawing machine"], prod_avail: ["Rod breakdown wire 1.8 mm to 3.5 mm",
             "Intermediate wire 1 mm to 1.6 mm",
-            "Fine wire 0.3 mm to 0.8 mm"], application: ["Extra flexible conductor for building wire, panel wire and welding cable",
+            "Fine wire 0.3 mm to 0.8 mm"], application: ["Flexible conductor for building wire and welding cable",
                 "HV, MV, LV Energy transmission with low heat development",
-                "High frequency communication cable application",
-                "Smooth and trouble free wire drawing during magnet wire enameling"]
+                "High frequency communication cable application"]
 }];
 export const appli_list = [{ id: '1', image: 'https://i.ibb.co/Jj59pLY/management-1031985.jpg', name: "Cables", short: 'Wire & Cable manufacturing with JDJ’s copper is highly beneficial due to its high purity and high electrical conductivity.' }, { id: '2', image: 'https://i.ibb.co/xDC2Scc/mika-baumeister-Sk-Uk-Z2au-N4-E-unsplash.jpg', name: "motors", short: 'JDJ’s oxygen free copper possesses a higher electrical and thermal conductivity which supports compact coil design in e-motors in electric vehicles.' }, { id: '3', image: 'https://i.ibb.co/HztxfyP/iqram-o-dowla-shawon-Wk2to7-RYKHo-unsplash.jpg', name: "Transformers", short: 'JDJ provide the maximum peace of mind to the transformer designers and operators by minimizing partial discharge & hot spots.' }, { id: '4', image: 'https://i.ibb.co/3TtcWX4/alternative-21761.jpg', name: "solar panels", short: 'JDJ’s upcast copper is highly suitable for PV interconnect ribbon and Bus bar ribbon application in solar panel system for its longer life.' }, { id: '5', image: 'https://i.ibb.co/dfwr2PY/storm-2496251.jpg', name: "Lightning Protection", short: 'JDJ’s continuously extruded bare copper tape is the brand of choice by the electrical installers & contractors as an earthing conductor for lightning protection.' }];
