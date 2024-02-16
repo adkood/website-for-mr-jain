a509603fa8522f60f06a5d008a5dbf5e5d093f49
adkood <ashutoshnegi195@gmail.com>
2024-02-16 12:11:32 +0530

    copy functionality implemented

--- client/src/components/ContactUs/ContactUs.js
+++ client/src/components/ContactUs/ContactUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../ContactUs/ContactUs.module.css';
+import copy from 'clipboard-copy';
 import { GiMoebiusStar } from "react-icons/gi";
 import { FaSquarePhone } from "react-icons/fa6";
 import { MdAttachEmail } from "react-icons/md";
@@ -7,6 +8,12 @@ import { FaAddressBook } from "react-icons/fa";
 import { MdFileCopy } from "react-icons/md";
 
 const ContactUs = React.memo(() => {
+
+    const handleCopyClick = (copyText) => {
+        copy(copyText);
+        alert('Text copied to clipboard!');
+    };
+
     return (
         <div id='contact-us' className={styles.ContactUs}>
             <section className={styles.top}>
@@ -22,7 +29,7 @@ const ContactUs = React.memo(() => {
                     <section className={styles.middle__item__right}>
                         <span>+91 9205186548</span>
                     </section>
-                    <section className={styles.middle__item__copy}>
+                    <section className={styles.middle__item__copy} onClick={() => handleCopyClick("+91 9205186548")}>
                         <MdFileCopy />
                     </section>
                 </section>
@@ -34,7 +41,7 @@ const ContactUs = React.memo(() => {
                     <section className={styles.middle__item__right}>
                         <span>Info.jdjain@gmail.com</span>
                     </section>
-                    <section className={styles.middle__item__copy}>
+                    <section className={styles.middle__item__copy} onClick={() => handleCopyClick("Info.jdjain@gmail.com")}>
                         <MdFileCopy />
                     </section>
                 </section>
@@ -46,7 +53,7 @@ const ContactUs = React.memo(() => {
                     <section className={styles.middle__item__right}>
                         <span>JDJ ALLOYS PVT LTD, Kundali village, Kundli, Haryana</span>
                     </section>
-                    <section className={styles.middle__item__copy}>
+                    <section className={styles.middle__item__copy} onClick={() => handleCopyClick("JDJ ALLOYS PVT LTD, Kundali village, Kundli, Haryana")}>
                         <MdFileCopy />
                     </section>
                 </section>
