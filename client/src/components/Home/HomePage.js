9d20df20265013c019707307298ba136aeb12b5c
adkood <ashutoshnegi195@gmail.com>
2024-02-22 22:56:50 +0530

    new home page

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = React.memo(() => {
 
     return (
         <div id="name" className={style.home}>
-            <IoMdStar className={style.star} size="2rem" color="rgb(238, 236, 236)" />
+            <IoMdStar className={style.star} size="2rem" color="white" />
             <div className={style.cont}></div>
             <div className={style.content}>
                     JD Jain and Company proudly presents its manufacturing unit JDJ Alloys as it embarks on a groundbreaking venture.
